5a23079bad3b4d5d3c620eba5c6c65234fbca210
mrgromov <mailrxtx@gmail.com>
2019-02-24 00:17:19 +0400

    smooth scrolling and sidenav open-close added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,34 @@
 import "../assets/styles/main.scss"
+import $ from "jquery"
+
 console.info(`[${new Date().toISOString()}] Page loaded!`)
+
+$(document).ready(function() {
+    // Add smooth scrolling to all links
+    $("a").on("click", function(event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "" && this.pathname === "/") {
+            // Prevent default anchor click behavior
+            event.preventDefault()
+            // Store hash
+            var hash = this.hash
+            $("html, body").animate(
+                {
+                    scrollTop: $(hash).offset().top
+                },
+                600,
+                () => {
+                    // Add hash (#) to URL when done scrolling (default click behavior)
+                    window.location.hash = hash
+                }
+            )
+        }
+    })
+
+    $(".cmd_sidenav_open").on("click", function(event) {
+        $(".sidenav").addClass("sidenav_opened")
+    })
+    $(".cmd_sidenav_close").on("click", function(event) {
+        $(".sidenav").removeClass("sidenav_opened")
+    })
+})
